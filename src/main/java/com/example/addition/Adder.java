030b91d448891cdc38bc9421e6195aa5092e7ef2
Neelakanta-sm <neelakanta.reddy@smartbear.com>
2025-07-10 14:41:36 +0530

    Added new methods

--- src/main/java/com/example/addition/Adder.java
+++ src/main/java/com/example/addition/Adder.java
@@ -1,6 +1,10 @@
 package com.example.addition;
 
 public class Adder {
+
+    public static void main(String[] args) {
+        System.out.println(add(5,4));
+    }
     public static int add(int a, int b) {
         return a + b;
     }
